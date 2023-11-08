b29472c78057aa7fc8d10b14fe891b1f7e18b07f
Manasa6991 <141348479+Manasa6991@users.noreply.github.com>
2023-11-08 11:46:26 +0530

    mywebhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("webhook triggered");
     System.out.println("success");
+    System.out.println("Done!");
   }
 }
